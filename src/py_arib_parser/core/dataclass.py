24edfb21d04c0bd773831276166d393dfe8d8afa
yuta0306 <yuta.20010306@gmail.com>
2023-08-29 17:25:46 +0900

    __repr__

--- src/py_arib_parser/core/dataclass.py
+++ src/py_arib_parser/core/dataclass.py
@@ -8,6 +8,14 @@ class Time(object):
     second: int
     millisecond: int
 
+    def __repr__(self) -> str:
+        return f"""Time(
+    hour        = {self.hour}
+    minute      = {self.minute}
+    second      = {self.second}
+    millisecond = {self.millisecond}
+)"""
+
 
 @dataclass
 class Text(object):
@@ -15,9 +23,25 @@ class Text(object):
     size: int | None = None
     color: str | None = None
 
+    def __repr__(self) -> str:
+        return f"""Text(
+    content = {self.content}
+    size    = {self.size}
+    color   = {self.color}
+)"""
+
 
 @dataclass
 class Caption(object):
     start: Time
     end: Time
     text: list[Text]
+
+    def __repr__(self) -> str:
+        start = self.start
+        end = self.end
+        return f"""Caption(
+    start = {start.hour:02d}:{start.minute:02d}:{start.second:02d}.{start.millisecond:03d}
+    end   = {end.hour:02d}:{end.minute:02d}:{end.second:02d}.{end.millisecond:03d}
+    text  = {self.text}
+)"""
